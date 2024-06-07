1eb2e6815910710a354dc54d3ab98319716cfdb7
Eralpozcan <eralpozcans@gmail.com>
2024-06-07 14:22:43 +0300

    update: Update deps., workflows  and disabled deepsource

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,52 @@
 export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: false },
+
+  app: {
+    head: {
+      title: 'Commite',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8'},
+        { name: 'viewport', content: 'width=device-width,initial-scale=1' },
+        { name: 'googlebot', content: 'index, follow' },
+        { name: 'author', content: 'Commite.dev' },
+        { name: 'language', content: 'English' },
+        { hid: 'robots', name: 'robots', content: 'index, follow'},
+        { name: 'application-name', content: 'Commite' },
+        { name: 'description', content: 'Community for developers and job seekers' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32' },
+        { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' },
+      ],
+      noscript: [
+        { children: 'JavaScript is required' }
+      ]
+    }
+  },
   modules: [
-    'nuxt-icon',
     '@nuxtjs/tailwindcss',
-    "@nuxtjs/i18n",
+    '@nuxtjs/i18n',
     '@vueuse/nuxt',
-    "@nuxt/eslint",
-    "nuxt-gtag"
-  ]
+    '@nuxt/eslint',
+    'nuxt-icon',
+    'nuxt-gtag',
+    '@nuxtjs/web-vitals',
+    '@nuxtjs/color-mode',
+    '@pinia/nuxt'
+  ],
+  gtag: {
+    id: process.env.NUXT_PUBLIC_GA_ID,
+    loadingStrategy: "async"
+  },
+  webVitals: {
+    ga: {
+      id: `${process.env.NUXT_PUBLIC_GA_ID}`,
+    },
+  }
 })
